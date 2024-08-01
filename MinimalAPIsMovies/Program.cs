d40c8c800a1f6759ee2b77f03b4d47b95ec04e1d
Esteban Alvarenga <estebanalvarenga2002@gmail.com>
2024-08-01 17:02:30 -0600

    knowing enviroment

--- MinimalAPIsMovies/Program.cs
+++ MinimalAPIsMovies/Program.cs
@@ -1,6 +1,40 @@
+using MinimalAPIsMovies.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Services zone - Begin
+
+// Services zone - End
+
 var app = builder.Build();
 
+// Middlewares zone - Begin
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/genres", () =>
+{
+    var genres = new List<Genre>()
+    {
+        new Genre
+        {
+            Id = 1,
+            Name = "Drama",
+        },
+        new Genre
+        {
+            Id = 2,
+            Name = "Comedy",
+        },
+        new Genre
+        {
+            Id = 3,
+            Name = "Horror",
+        },
+    };
+
+    return genres;
+});
+
+// Middlewares zone - End
 
 app.Run();
