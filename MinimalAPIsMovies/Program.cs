2a7feb5f9e29890a8021c1feb685afac7b0be34d
Esteban Alvarenga <estebanalvarenga2002@gmail.com>
2024-11-06 02:20:14 -0600

    feat(program, error): Save exceptions in db

--- MinimalAPIsMovies/Program.cs
+++ MinimalAPIsMovies/Program.cs
@@ -1,5 +1,7 @@
 using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
 using MinimalAPIsMovies.Endpoints;
+using MinimalAPIsMovies.Entities;
 using MinimalAPIsMovies.Repositories;
 using MinimalAPIsMovies.Services;
 
@@ -14,11 +16,14 @@ builder.Services.AddScoped<IGenresRepository, GenresRepository>();
 builder.Services.AddScoped<IActorsRepository, ActorsRepository>();
 builder.Services.AddScoped<IMoviesRepository, MoviesRepository>();
 builder.Services.AddScoped<ICommentsRepository, CommentsRepository>();
+builder.Services.AddScoped<IErrorsRepository, ErrorsRepository>();
 
 builder.Services.AddTransient<IFileStorage, AzureFileStorage>();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+builder.Services.AddProblemDetails();
 // Services zone - End
 
 var app = builder.Build();
@@ -26,7 +31,33 @@ var app = builder.Build();
 // Middlewares zone - Begin
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseExceptionHandler(exceptionHandlerApp => exceptionHandlerApp.Run(async context =>
+{
+    var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+    var exception = exceptionHandlerFeature?.Error!;
+
+    var error = new Error();
+    error.Date = DateTime.UtcNow;
+    error.ErrorMessage = exception.Message;
+    error.StackTrace = exception.StackTrace;
+
+    var repository = context.RequestServices.GetRequiredService<IErrorsRepository>();
+    await repository.Create(error);
+
+    await Results.BadRequest(new 
+    { 
+        type = "error", 
+        message = "an unexpected exception has ocurred", 
+        status = 500 
+    }).ExecuteAsync(context);
+}));
+app.UseStatusCodePages();
 app.UseOutputCache();
+
+app.MapGet("/error", () =>
+{
+    throw new InvalidOperationException("Example error");
+});
 
 app.MapGroup("/genres").MapGenres();
 app.MapGroup("/actors").MapActors();
